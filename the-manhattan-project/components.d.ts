05e3a288ac15e9a80f20b951f1c1327da3e51987
Andrew Rubalcaba <arubalcaba@806technologies.com>
2024-06-04 13:27:29 -0500

    Lesson 3 complete - User Profile Menu with Navbar

--- the-manhattan-project/components.d.ts
+++ the-manhattan-project/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    DefaultView: typeof import('./src/components/DefaultView.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- the-manhattan-project/src/App.vue
+++ the-manhattan-project/src/App.vue
@@ -1,9 +1,34 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="Application bar">
+      <template #append>
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-avatar  v-bind="props" size="50" color="blue">AR</v-avatar>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in ['Profile', 'Settings', 'Logout']"
+              :key="index"
+              :value="index"
+            >
+              <v-list-item-title>{{ item }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </template>
+    </v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+     <router-view></router-view>
     </v-main>
-  </v-app>
+  </v-layout>
 </template>
 
 <script lang="ts" setup>
