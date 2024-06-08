b6b42541089a64cb62c71672638e5506bc24b84b
Andrew Rubalcaba <arubalcaba@806technologies.com>
2024-06-08 09:40:39 -0500

    Completed Lesson 5: Table Component and some CSS Utilities
    Completed Lesson 5: Table Component and some CSS Utilities

--- the-manhattan-project/src/components/PostView.vue
+++ the-manhattan-project/src/components/PostView.vue
@@ -1,5 +1,35 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+const posts = ref([
+    {id: 1,title: 'Post 1', author: 'John Doe'},
+    {id: 2,title: 'Post 2', author: 'Juan Doe'},
+    {id: 3,title: 'Post 3', author: 'Jane Doe'},
+    {id: 4,title: 'Post 4', author: 'SanJui Doe'},
+
+])
+</script>
 <template>
     <div>
-        Post
+        <h1>Post</h1>
+        <v-table
+        :height="300" :fixed-header="true" :hover="true"
+        >
+            <thead>
+                <tr>
+                    <th class="text-left">
+                        Title
+                    </th>
+                    <th class="text-left">
+                        Author
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="post in posts" :key="post.id">
+                    <td>{{ post.title }}</td>
+                    <td>{{ post.author }}</td>
+                </tr>
+            </tbody>
+        </v-table>
     </div>
 </template>
