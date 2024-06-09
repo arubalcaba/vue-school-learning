c8994e84fa7cdcfd8092b475d8ea2ef6a3300b3a
Andrew Rubalcaba <arubalcaba@806technologies.com>
2024-06-09 13:01:25 -0500

    Lesson 6 - Data Table Component
    Lesson 6 - Data Table Component

--- the-manhattan-project/src/components/PostView.vue
+++ the-manhattan-project/src/components/PostView.vue
@@ -1,35 +1,22 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 const posts = ref([
-    {id: 1,title: 'Post 1', author: 'John Doe'},
-    {id: 2,title: 'Post 2', author: 'Juan Doe'},
-    {id: 3,title: 'Post 3', author: 'Jane Doe'},
-    {id: 4,title: 'Post 4', author: 'SanJui Doe'},
+    { id: 1, title: 'Post 1', author: 'John Doe' },
+    { id: 2, title: 'Post 2', author: 'Juan Doe' },
+    { id: 3, title: 'Post 3', author: 'Jane Doe' },
+    { id: 4, title: 'Post 4', author: 'SanJui Doe' },
 
 ])
+const selected = ref([])
+const search = ref('')
 </script>
 <template>
     <div>
         <h1>Post</h1>
-        <v-table
-        :height="300" :fixed-header="true" :hover="true"
-        >
-            <thead>
-                <tr>
-                    <th class="text-left">
-                        Title
-                    </th>
-                    <th class="text-left">
-                        Author
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="post in posts" :key="post.id">
-                    <td>{{ post.title }}</td>
-                    <td>{{ post.author }}</td>
-                </tr>
-            </tbody>
-        </v-table>
+        <v-text-field v-model="search" label="Search" prepend-inner-icon="mdi-magnify" variant="outlined" hide-details
+            single-line></v-text-field>
+        <v-data-table :items="posts" show-select v-model="selected" :search="search">
+
+        </v-data-table>
     </div>
 </template>
