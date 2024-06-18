454f1d5aae9978e6a09970e39e9dd65fabff42ca
Andrew Rubalcaba <arubalcaba@806technologies.com>
2024-06-18 17:13:35 -0500

    Finished Lesson 7 adding in Button and Dialog
    Finished Lesson 7 adding in Button and Dialog

--- the-manhattan-project/src/components/PostView.vue
+++ the-manhattan-project/src/components/PostView.vue
@@ -16,7 +16,29 @@ const search = ref('')
         <v-text-field v-model="search" label="Search" prepend-inner-icon="mdi-magnify" variant="outlined" hide-details
             single-line></v-text-field>
         <v-data-table :items="posts" show-select v-model="selected" :search="search">
+            <template #item.title="{ item }">
+                <v-dialog fullscreen transition="dialog-bottom-transition">
+                    <template v-slot:activator="{ props: activatorProps }">
+                        <button v-bind="activatorProps" color="surface-variant"
+                            variant="flat">{{ item.title }}</button>
+                    </template>
 
+                    <template v-slot:default="{ isActive }">
+                        <v-card :title="item.title">
+                            <v-card-text>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua.
+                            </v-card-text>
+
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+
+                                <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </template>
+                </v-dialog>
+            </template>
         </v-data-table>
     </div>
 </template>
