790375000c0b95151cd0030b293de68f4b3e4243
Andrew Rubalcaba <arubalcaba@806technologies.com>
2024-06-19 15:09:19 -0500

    Lesson 10 - Feedback Components
    Lesson 10 - Feedback Components

--- the-manhattan-project/src/components/PostView.vue
+++ the-manhattan-project/src/components/PostView.vue
@@ -11,9 +11,23 @@ const posts = ref([
 const selected = ref([])
 const search = ref('')
 const postForm = ref();
+const postSaved = ref(false)
 </script>
 <template>
     <div>
+        <v-alert variant="tonal" closable type="success" title="Post Updated" v-model="postSaved">
+        </v-alert>
+
+        <v-snackbar v-model="postSaved" :timeout="4000">
+            <div class="d-flex align-center">
+                <v-icon icon="mdi mdi-check-circle" class="text-white pr-3">
+                </v-icon>
+                Post Saved!
+            </div>
+
+        </v-snackbar>
+
+
         <h1>Post</h1>
         <v-text-field v-model="search" label="Search" prepend-inner-icon="mdi-magnify" variant="outlined" hide-details
             single-line></v-text-field>
@@ -21,25 +35,22 @@ const postForm = ref();
             <template #item.title="{ item }">
                 <v-dialog fullscreen transition="dialog-bottom-transition">
                     <template v-slot:activator="{ props: activatorProps }">
-                        <button v-bind="activatorProps" color="surface-variant"
-                            variant="flat">{{ item.title }}</button>
+                        <button v-bind="activatorProps" color="surface-variant" variant="flat">{{ item.title }}</button>
                     </template>
 
                     <template v-slot:default="{ isActive }">
                         <v-card :title="item.title">
                             <v-card-text>
-                                <PostForm ref="postForm" :post="item" @submit="isActive.value = false"/>
+                                <PostForm ref="postForm" :post="item"
+                                    @submit="isActive.value = false; postSaved = true" />
                             </v-card-text>
 
                             <v-card-actions>
                                 <v-spacer></v-spacer>
 
                                 <v-btn text="Cancel" @click="isActive.value = false"></v-btn>
-                                <v-btn color="primary"
-                                variant="flat"
-                                text="Save Post"
-                                @click="
-                                postForm.submit()"></v-btn>
+                                <v-btn color="primary" variant="flat" text="Save Post" @click="
+                                    postForm.submit()"></v-btn>
                             </v-card-actions>
                         </v-card>
                     </template>
